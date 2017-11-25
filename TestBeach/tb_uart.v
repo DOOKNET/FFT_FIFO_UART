45a377dcaf128152bebd3135aa70f5f95ad94dcd
DOOKNET <zichen.ding@outlook.com>
2017-11-25 14:36:43 +0800

    版本3.0
    1.在之前的基础上加了uart使能信号仿真

--- TestBeach/tb_uart.v
+++ TestBeach/tb_uart.v
@@ -4,7 +4,7 @@ module	tb_uart();
 
 reg 	[31:0]	data = 32'b10101010_01010101_10101010_01010101;		//0001_0010_0011_0001_0010_0011_0001_0010		h12312312
 reg		sclk;
-reg		uart_en = 1;
+reg		uart_en;
 
 wire	wr_clk;
 wire 	tx;
@@ -12,6 +12,13 @@ wire 	tx;
 initial		sclk = 1;
 always	#5	sclk = ~sclk;
 //---------------------------//
+initial		uart_en = 1;
+always	begin
+	#1700000
+	uart_en = 0;
+
+end
+
 
 uart_tx			uart_tx_inst(
 	.clk		(sclk),
